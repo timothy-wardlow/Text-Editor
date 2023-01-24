c8ae2a052781328bf039a165bd51c639e8554e8e
Timothy Wardlow <wardlowtimothy@icloud.com>
2023-01-24 16:14:22 -0600

    working on client side, still testing wip

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,29 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
-
+export const putDb = async (content) => {
+  console.error('putDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  const request = store.put({ content: content });
+  const result = await request;
+  console.log('data saved to db', result);
+};
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.error('getDb not implemented');
+  const jateDb = await openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.getAll();
+  const result = await request;
+  if (result) {
+    console.log("result.value", result.content);
+    return result.content
+  } else {
+    console.error("getDb not implemented");
+  }
+}
 
 initdb();
